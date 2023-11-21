2e38e4d08440afe283d8d88cf18da81c0a7b9db2
Yash Mehta <yashmehta001@yahoo.in>
2023-11-21 23:13:00 +0530

    21/11/2023

--- 16-fast-react-pizza/starter/fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/App.jsx
@@ -1,39 +1,38 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
-import Cart from './features/cart/Cart'
-import CreateOrder from './features/order/CreateOrder'
-import Order from './features/order/Order'
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
-{
-  element:<AppLayout/>,
-  children:[
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/menu",
-      element: <Menu />,
-    },
-    {
-      cart: "/cart",
-      element: <Cart />,
-    },
-    {
-      cart: "/order/new",
-      element: <CreateOrder />,
-    },
-    {
-      cart: "/order/:orderId",
-      element: <Order />,
-    },
-
-  ]
-}
-
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        cart: "/cart",
+        element: <Cart />,
+      },
+      {
+        cart: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        cart: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
