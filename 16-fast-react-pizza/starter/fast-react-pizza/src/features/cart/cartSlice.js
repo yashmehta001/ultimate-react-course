e6d14947b5228a3d03d3e330ea1c0b192f323fc3
Yash <yash.mehta@jklgroup.in>
2023-12-01 11:18:22 +0530

    12/1/2023

---
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.id !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.filter((item) => item.id === action.payload);
+      item.quanity++;
+
+      item.totalPrice = item.quanity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.filter((item) => item.id === action.payload);
+
+      item.quanity--;
+      item.totalPrice = item.quanity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+export default cartSlice.reducer;
