110ae6275752273a213657f8e90810d2ea4fa09a
Yash <yash.mehta@jklgroup.in>
2023-12-02 12:31:48 +0530

    02/12/2023

--- 16-fast-react-pizza/starter/fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/starter/fast-react-pizza/src/App.jsx
@@ -4,6 +4,7 @@ import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import {action as updateOrderAction} from './features/order/UpdateOrder'
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -37,6 +38,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action:updateOrderAction
       },
     ],
   },
