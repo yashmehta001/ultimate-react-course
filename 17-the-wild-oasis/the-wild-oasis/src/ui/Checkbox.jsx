5c45fe33983c2b0235fbfeeb525e1b3f360608a3
Yash Mehta <yashmehta001@yahoo.in>
2023-12-12 21:26:40 +0530

    12/12/2023

--- 17-the-wild-oasis/the-wild-oasis/src/ui/Checkbox.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/Checkbox.jsx
@@ -25,7 +25,6 @@ const StyledCheckbox = styled.div`
   }
 `;
 
-// eslint-disable-next-line react/prop-types
 function Checkbox({ checked, onChange, disabled = false, id, children }) {
   return (
     <StyledCheckbox>
