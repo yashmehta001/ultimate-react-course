2513cf6158d5b463c60f1deaf2775cbe83a5b67b
Yash <yash.mehta@jklgroup.in>
2024-01-18 16:18:38 +0530

    18-01-2024

--- 17-the-wild-oasis/the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from '../../ui/Modal'
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
     <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new Cabin
-      </Button>
-      {isOpenModal && (<Modal onClose={() => setIsOpenModal(false)}>
-        <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-      </Modal>)}
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
     </div>
   );
 }
