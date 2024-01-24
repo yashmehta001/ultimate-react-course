a7b060822f9c77427e928b8987d3b15026def2a0
Yash Mehta <yashmehta766@gmail.com>
2024-01-24 20:46:21 +0530

    24-01-2024

--- 17-the-wild-oasis/the-wild-oasis/src/ui/Filter.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/Filter.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -34,19 +35,31 @@ const FilterButton = styled.button`
   }
 `;
 
-function Filter (){
-  return(
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    if (searchParams.get("page")) searchParams.set("page", 1);
+
+    setSearchParams(searchParams);
+  }
+
+  return (
     <StyledFilter>
-      <FilterButton>
-        All
-      </FilterButton>
-      <FilterButton>
-        No Discount
-      </FilterButton>
-      <FilterButton>
-        With Discount
-      </FilterButton>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
     </StyledFilter>
-  )
+  );
 }
+
 export default Filter;
